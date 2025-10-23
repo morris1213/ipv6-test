be1bd287989fb1d6fcc352a87f7423a0eab19e28
lighting.launch <lighting.launch@gmail.com>
2025-10-23 08:14:09 +0000

    test2

--- src/pages/api/ipv6.json.js
+++ src/pages/api/ipv6.json.js
@@ -1,19 +1,13 @@
-import { lookup } from 'node:dns/promises';
+// We are removing the node:dns/promises lookup because it's not available in the Cloudflare runtime.
+// The API will now only report the client's IP address type.
 
 export const prerender = false;
 
 export const GET = async ({ clientAddress }) => {
     const isIPv6 = clientAddress ? clientAddress.includes(':') : false;
-    let hasIPv6Dns = false;
 
-    try {
-        const { address } = await lookup('ipv6.google.com');
-        if (address) {
-            hasIPv6Dns = true;
-        }
-    } catch (error) {
-        // The lookup will fail if the DNS resolver can't handle IPv6
-    }
+    // The DNS check has been removed for Cloudflare compatibility.
+    const hasIPv6Dns = false; // Defaulting to false as we can't perform the check.
 
     return new Response(JSON.stringify({ ipv6: isIPv6, ipv6Dns: hasIPv6Dns }), {
         headers: { 'Content-Type': 'application/json' },
